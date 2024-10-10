65141fd421a9069236293a38f8ab49b523562ff8
Andreas <andreas.brandenborg@live.dk>
2024-10-10 11:32:58 +0200

    Crated a way to show history of guesses. added confetti. added a too high or too low feature to make the guessing easier

--- script.js
+++ script.js
@@ -1,64 +1,60 @@
-// When clicking the Check! button check if what the user wrote in the input matches the number they have to guess
-// Random Number
 
 //Random Number
 const randomNumber = Math.floor(Math.random() * 20);
 console.log(randomNumber)
 
-//Global scope variables
+//Dom
 const userGuess = document.querySelector("input.guess")
 const checkButton = document.querySelector("button.check")
 const highscore = document.querySelector("span.highscore")
 const questionMarkNumber = document.querySelector("div.number")
+
+//score
 let score = 0
 
 //Checks if checkButton is clicked and runs if statements
 checkButton.addEventListener("click", () => {
+
     //Shows alert if userGuess is not between 0-20
     const userGuessValue = parseInt(userGuess.value)
-    if (userGuessValue > 20 || userGuessValue < 0) {
-        alert("Number needs to be between 0-20")
+    if (userGuess > 20 || userGuess < 0 || !Number.isInteger(userGuessValue)) {
+        alert("Input needs to be a number between 0 and 20")
         score--
         userGuess.value = ""
+        console.log(userGuessValue)
     }
-   else if (userGuessValue !== Number) {
-        alert("Needs to be a number")
-    }
+
     //if userGuess is correct clears input and shows randomNumber in div.number
     else if (userGuessValue === randomNumber) {
         userGuess.value = "";
         questionMarkNumber.textContent = `${randomNumber}`;
+        confetti()
 
         //if score is smaller than highscore sets highscore to current score
         if (parseInt(highscore.textContent) < score) {
             highscore.textContent = `${score}`
         }
-    //If userGuess is not equal to randomNumber increase score and clears input
 
+    //If userGuess is not equal to randomNumber increase score clears input and shows guessed number
     } else {
         score++
         const currentScore = document.querySelector("span.score")
         currentScore.textContent = `${score}`
         userGuess.value = "";
-
+        document.querySelector("ul.guesses-list").insertAdjacentHTML("afterbegin", `<li class="current-guesses-list">${userGuessValue}</li>`)
+        if (userGuessValue > randomNumber) {
+            alert("Too High")
+        }   else {
+            alert("Too Low")
+        }
     }
 })
 
-//If again is clicked refresh page
+
+//If again is clicked refresh page and start over
 const againBtn = document.querySelector("button.again")
 againBtn.addEventListener("click", () => {
     location.reload();
     }
 )
 
-
-
-// If the user writes a number that is not between 1 - 20, then an error message should be shown to the user. Or if the user writes something that is not a number
-
-// Create a way to show what numbers have been guessed so far. This is like a history of guesses
-
-// Optional - Add confetti when the correct number is guessed
-
-// Optional - Play diferent sounds when the correct or wrong number is guessed
-
-// Add a feature you think could be fun!
